91c020cffe94d519ac2edc677d6787cf73d7ac33
Nooa Korhonen <nooa_korhonen@saunalahti.fi>
2026-08-20 10:22:22 +0300

    tuntiteht 1

---
+++ tuntiteht/teht1.py
@@ -0,0 +1,6 @@
+nimi = input("Anna nimi:" )
+adjektiivi = input("Anna jokin adjektiivi: ")
+
+print(nimi + " oli " + adjektiivi + " ritari, joka asui hienossa linnassa. ")
+print("Eräänä aamuna " + adjektiivi + " " + nimi + " heräsi kamalaan meteliin: Lohikäärme oli hyökkäämässä kylään.")
+print("Vain " + adjektiivi + " " + nimi + " voisi pelastaa kylän asukkaat.")
